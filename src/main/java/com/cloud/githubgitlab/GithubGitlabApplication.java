6da99d4ed65065719821e82a288347572fdcfcb2
imdanaa <jangs1920@gmail.com>
2024-12-20 12:21:28 +0900

    sync 프로젝트 업로드

---
+++ src/main/java/com/cloud/githubgitlab/GithubGitlabApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.githubgitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubGitlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubGitlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloud/githubgitlab/GithubGitlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.githubgitlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubGitlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
